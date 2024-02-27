ebf05ca5283d97562f72f3c8248f7766f857a7b7
gjustoo <zzxxzz113@gmail.com>
2024-02-27 20:40:45 +0100

    Retrieved PORT from OS.ENV

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"os"
+
 	"github.com/gjustoo/portfolio-go/handler"
 	"github.com/labstack/echo/v4"
 )
@@ -10,9 +12,14 @@ func main() {
 	app := echo.New()
 	userHandler := handler.LayoutHandler{}
 
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		port = "8080"
+	}
 	app.Static("/static/*,", "static")
 	app.GET("/", userHandler.HandleLayoutShow)
 	//Move this por to a .ENV file
-	app.Start(":3000")
+	app.Start(":" + port)
 
 }
